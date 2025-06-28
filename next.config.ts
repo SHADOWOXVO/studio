54787f2cf7d352ac46aad7f95c502134db035f50
shadow xv <zoooohto@gmail.com>
2025-06-28 08:51:16 +0000

    problem still there can we try another method to download the app on pho

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,48 @@ const withPWA = withPWAInit({
     disable: process.env.NODE_ENV === "development",
     register: true,
     skipWaiting: true,
+    runtimeCaching: [
+        // Cache Google Fonts
+        {
+          urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+          handler: "CacheFirst",
+          options: {
+            cacheName: "google-fonts",
+            expiration: {
+              maxEntries: 4,
+              maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+            },
+          },
+        },
+        // Cache images
+        {
+          urlPattern: /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
+          handler: "CacheFirst",
+          options: {
+            cacheName: "images",
+            expiration: {
+              maxEntries: 60,
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+            },
+          },
+        },
+        // Cache all other pages and assets
+        {
+          urlPattern: ({ url }) => url.origin === self.location.origin,
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "pages-and-assets",
+            networkTimeoutSeconds: 10,
+            expiration: {
+              maxEntries: 60,
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+            },
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+    ]
 });
 
 
