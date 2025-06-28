f479c852470864b67f4f0f48967f2ccf062edf81
shadow xv <zoooohto@gmail.com>
2025-06-28 08:33:35 +0000

    same problem however i noticed when i become online it askes me if i wan

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,6 @@ const withPWA = withPWAInit({
     disable: process.env.NODE_ENV === "development",
     register: true,
     skipWaiting: true,
-    fallbacks: {
-        document: "/_offline",
-    }
 });
 
 
