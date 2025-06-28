1411df26ffc5a34b639944b3770a94e15245f2ee
shadow xv <zoooohto@gmail.com>
2025-06-28 08:40:10 +0000

    no still the same problem cant we use another way to download the app on

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,42 @@ const withPWA = withPWAInit({
     disable: process.env.NODE_ENV === "development",
     register: true,
     skipWaiting: true,
+    runtimeCaching: [
+        {
+            urlPattern: ({ request }) => request.mode === 'navigate',
+            handler: 'NetworkFirst',
+            options: {
+                cacheName: 'pages',
+                expiration: {
+                    maxEntries: 60,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+                },
+                networkTimeoutSeconds: 3,
+            },
+        },
+        {
+            urlPattern: /\.(?:js|css|webp|png|svg|ico)$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+                cacheName: 'static-assets',
+                expiration: {
+                    maxEntries: 60,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+                },
+            },
+        },
+        {
+            urlPattern: ({ request }) => request.destination === 'font',
+            handler: 'CacheFirst',
+            options: {
+                cacheName: 'fonts',
+                expiration: {
+                    maxEntries: 30,
+                    maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year
+                },
+            },
+        },
+    ],
 });
 
 
